0b9ada9f6ec3102fccb7488ebf23314632382ae8
Xuemin Zhou <zhou0223@algonquinlive.com>
2023-05-19 14:17:03 -0400

    removed 8 lines

--- app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/ericsandroidlabs/MainActivity.java
@@ -15,12 +15,4 @@ public class MainActivity extends AppCompatActivity {
         //loads an XML file on the page
         setContentView(  R.layout.activity_main   );
     }
-    //add 8 blank spaces
-
-
-
-
-
-
-
 }
