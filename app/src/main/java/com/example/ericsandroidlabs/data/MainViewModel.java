49dcd56282d448529f9c9fdc3f6567cd33a74a34
Xuemin Zhou <zhou0223@algonquinlive.com>
2023-05-21 23:04:05 -0400

    finished lab 2

--- app/src/main/java/com/example/ericsandroidlabs/data/MainViewModel.java
+++ app/src/main/java/com/example/ericsandroidlabs/data/MainViewModel.java
@@ -1,10 +1,10 @@
 package com.example.ericsandroidlabs.data;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 public class MainViewModel extends ViewModel{
-    public String theText = "Hello World";
-    public String buttonText= "Click Me";
-    public String editText = "Edit text";
-    public String imageText = "Imageview text";
+    public MutableLiveData<String> editText = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
     //public String edtString;
     }
 
--- app/src/main/java/com/example/ericsandroidlabs/ui/MainActivity.java
+++ app/src/main/java/com/example/ericsandroidlabs/ui/MainActivity.java
@@ -33,56 +33,35 @@ public class MainActivity extends AppCompatActivity {
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         //loads the screen
         setContentView(variableBinding.getRoot());
-        //old way of getting widgets
-        TextView theText = variableBinding.theText;
-        Button myButton = variableBinding.theButton;
-        EditText myEdit = variableBinding.theEdit;
-        CheckBox myCheckbox = variableBinding.myCheckbox;
-        Switch mySwitch = variableBinding.mySwitch;
-        RadioButton myRadioButton = variableBinding.myRadioButton;
-        ImageButton myImageButton = variableBinding.myImageButton;
-        //TextView theText = findViewById(R.id.theText );
-        //Button myButton = findViewById( R.id.theButton );
-        //EditText myEdit = findViewById( R.id.theEdit );
-        //CheckBox myCheckbox = findViewById( R.id.myCheckbox );
-        
-        //anCheckedChanged()
-        myCheckbox.setOnCheckedChangeListener( (a,b) ->{
-            theText.setText("The checkbox is on?" +b);
-        });
-        //when only 1 line of code between{}
-        mySwitch.setOnCheckedChangeListener( (a, b) ->{
-            theText.setText("The switch is on?" +b);
+
+        variableBinding.theButton.setOnClickListener(click -> {
+            model.editText.postValue(variableBinding.myeditText.getText().toString());
         });
 
-        myRadioButton.setOnCheckedChangeListener( (a, b) ->{
-            theText.setText("The myRadioButton is on?" +b);
+        model.editText.observe(this, s -> {
+            String editTextValue = s != null ? s : "";
+            variableBinding.myeditText.setText(editTextValue);
+            variableBinding.theText.setText(editTextValue);
         });
 
-
-        theText.setText( model.theText );
-        myButton.setText( model.buttonText );
-        myEdit.setText( model.editText );
-        myImageButton.setContentDescription(model.imageText);
-
-
-        myButton.setOnClickListener((v) -> {
-            model.theText = "You clicked the button";
-            model.buttonText = "Something new here";
-            model.editText = "My edit text";
-            model.imageText = "My image text";
-
-            //this gets run when you click the button
-            theText.setText(model.theText);
-            myButton.setText(model.buttonText);
-            myEdit.setText(model.editText);
-            myImageButton.setContentDescription(model.imageText);
-
-            // Show the Toast
-            Context context = getApplicationContext();
-            CharSequence text = "Button clicked!";
-            int duration = Toast.LENGTH_SHORT;
-            Toast.makeText(context, text, duration).show();
+        //
+        model.isSelected.observe(this, selected -> {
+            Toast.makeText(this,"The value now is :"+selected.toString(),Toast.LENGTH_SHORT).show();
+                    variableBinding.myCheckbox.setChecked(selected);
+                    variableBinding.mySwitch.setChecked(selected);
+                    variableBinding.myRadioButton.setChecked(selected);
                 });
-        }
+        variableBinding.myCheckbox.setOnCheckedChangeListener((btn, isChecked) -> {
+                model.isSelected.postValue(isChecked);
+            });
+        variableBinding.mySwitch.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        });
+        variableBinding.myRadioButton.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        });
+        variableBinding.myImageButton.setOnClickListener((click) -> {
+            Toast.makeText(this,"the width ="+variableBinding.myImageButton.getWidth()+"and height ="+variableBinding.myImageButton.getHeight(),Toast.LENGTH_SHORT).show();
+        });
+    }
 }
