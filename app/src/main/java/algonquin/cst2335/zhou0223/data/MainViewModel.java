39a9c2501dc37e79da8bb001184cb4d974b27ac7
Xuemin Zhou <zhou0223@algonquinlive.com>
2023-05-26 14:31:14 -0400

    Finished step 3 /26

--- app/src/main/java/algonquin/cst2335/zhou0223/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/zhou0223/data/MainViewModel.java
@@ -2,7 +2,4 @@ package algonquin.cst2335.zhou0223.data;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-    public class MainViewModel extends ViewModel {
-        public MutableLiveData<String> editText = new MutableLiveData<>();
-        public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
-    }
+   
