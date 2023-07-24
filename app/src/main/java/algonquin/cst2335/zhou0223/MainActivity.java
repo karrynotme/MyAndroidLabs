84b4ba6e7f8025b77c6e5abf9fc823e4c5236734
Min <114263825+karrynotme@users.noreply.github.com>
2023-07-24 17:48:04 -0400

    lab 10 finished

--- app/src/main/java/algonquin/cst2335/zhou0223/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhou0223/MainActivity.java
@@ -1,91 +1,95 @@
 package algonquin.cst2335.zhou0223;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import algonquin.cst2335.zhou0223.databinding.ActivityMainBinding;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import algonquin.cst2335.zhou0223.databinding.ActivityMainBinding;
 
 /**
  * The main activity java class of the activity_main.xml
- * @author Xuemin
+ * @author Min
  * @version 1.0
  */
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding variableBinding;
-//    private MainViewModel model;
-
-    /**
-     * A function to check the password contains at least an uppercase, a lowercase,
-     * a number and a special character.
-     * @param pw The String object that we are checking
-     * @return  Return true if the password is complex enough
-     */
-    private boolean checkPasswordComplexity(String pw){
-        if(!pw.matches(".*[A-Z].*")){
-            Toast.makeText(this, "Missing an UpperCase letter", Toast.LENGTH_SHORT);
-            return false;
-        }
-        else if(!pw.matches(".*[a-z].*")){
-            Toast.makeText(this, "Missing a LowerCase letter", Toast.LENGTH_SHORT);
-            return false;
-        }
-        else if(!pw.matches(".*\\d.*")){
-            Toast.makeText(this, "Missing a Number", Toast.LENGTH_SHORT);
-            return false;
-        }
-        else if(!pw.matches(".*[#$%^&*!@?].*")){
-            Toast.makeText(this, "Missing a Special Character (#$%^&*!@?)", Toast.LENGTH_SHORT);
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-
-    //boolean isSpecialCharacter(char c){
-       // switch (c)
-        //{
-           // case '#':
-           // case'?':
-           // case'*':
-              //  return true;
-          //  default:
-               // return false;
-      //  }
-   // }
+    protected String cityName;
+    protected RequestQueue queue = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        //view binding
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());
+        queue = Volley.newRequestQueue(this);
+        ActivityMainBinding binding = ActivityMainBinding.inflate( getLayoutInflater() );
+        setContentView(binding.getRoot());
 
-        /** This holds the text at the centre of the screen*/
-        TextView tv = variableBinding.textView;
-        /** This hold the input of the password from the user */
-        EditText et = variableBinding.theEditText;
-        /** This holds the login in button*/
-        Button btn = variableBinding.button;
-
-        //click button to show text from edittext to text view
-        btn.setOnClickListener(click -> {
-            String password = et.getText().toString();
-            if(checkPasswordComplexity(password)){
-                tv.setText("Your password is complex enough");
+        binding.forecastButton.setOnClickListener(click -> {
+            cityName = binding.cityTextField.getText().toString();
+            String stringURL = null;
+            try {
+                stringURL = "https://api.openweathermap.org/data/2.5/weather?q=" + URLEncoder.encode(cityName, "UTF-8") + "&appid=7e943c97096a9784391a981c4d878b22&units=metric";
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
             }
-            else{
-                tv.setText("You shall not pass!");
-            }
+            //this goes in the button click handler:
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringURL, null,
+                    (response)->{
+                        try {
+                            JSONArray weather = response.getJSONArray("weather");
+                            JSONObject data = response.getJSONObject("main");
+                            binding.temp.setText("The current temperature is " + data.getString("temp"));
+                            binding.maxTemp.setText("The max temperature is " + data.getString("temp_max"));
+                            binding.minTemp.setText("The min temperature is " + data.getString("temp_min"));
+                            binding.humitidy.setText("The humidity is " + data.getString("humidity") + "%");
+                            binding.description.setText(weather.getJSONObject(0).getString("description"));
+                            String iconName = weather.getJSONObject(0).getString("icon");
+
+                            String imageUrl = "https://openweathermap.org/img/w/" + iconName + ".png";
+
+                            String pathname = getFilesDir() + "/" + iconName + ".png";
+                            File f = new File(pathname);
+                            if(f.exists()){
+                                binding.icon.setImageBitmap(BitmapFactory.decodeFile(pathname));
+                            }
+                            else {
+                                ImageRequest imgReq = new ImageRequest(imageUrl, new Response.Listener<Bitmap>() {
+                                    @Override
+                                    public void onResponse(Bitmap bitmap) {
+                                        binding.icon.setImageBitmap(bitmap);
+                                    }
+                                }, 1024, 1024, ImageView.ScaleType.CENTER, null, (error) -> {
+                                    int j = 0;
+                                });
+                                queue.add(imgReq);
+                            }
+                        }
+                        catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    },
+                    (error) ->
+                    {  int j =0;   }
+            );
+            queue.add(request);
         });
     }
 }
-
